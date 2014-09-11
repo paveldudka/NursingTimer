cf84b51aa2d7d38bb663bda8ea8ca82649ecec75
Pavel Dudka <dudka.pavel@gmail.com>
2014-09-11 11:15:48 -0700

    Initial commit

---
+++ app/src/main/java/trickyandroid/com/nursingtimer/widgets/PoopTimer.java
@@ -0,0 +1,32 @@
+package trickyandroid.com.nursingtimer.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import trickyandroid.com.nursingtimer.R;
+
+/**
+ * Created by paveld on 9/10/14.
+ */
+public class PoopTimer extends TimerLayout {
+    public PoopTimer(Context context) {
+        super(context);
+    }
+
+    public PoopTimer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PoopTimer(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public PoopTimer(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    int getIconResId() {
+        return R.drawable.poop;
+    }
+}
